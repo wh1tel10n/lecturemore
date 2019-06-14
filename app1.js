753c9ac4f2b3cf8453f0e55a125494046c28e76a
wh1tel10n <sm.whitelion@gmail.com>
2019-06-14 16:36:01 +0900

    Third Commit

--- app1.js
+++ app1.js
@@ -1,54 +1,22 @@
-
-
 var express = require("express");
-
 var http = require("http");
-
 var path = require("path");
-
-
-
 var app = express();
 
+	app.use(express.static(path.join(__dirname, 'public')));
 
+	app.get("/Login", (req, res)=> {
+		console.log("user login");
+		var uId = req.param("uId");
+		var uPw = req.param("uPw");
+		res.writeHead("200", {"Content-Type":"text/html;charset=utf8"});
+		res.write("user uId : " + uId);
+		res.write("<br>");
+		res.write("userd uPw : " + uPw);
+		res.end();
+	});
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
-
-app.get("/Login", function(req, res){
-
-	console.log("user login");
-
-	
-
-	var uId = req.param("uId");
-
-	var uPw = req.param("uPw");
-
-	
-
-	res.writeHead("200", {"Content-Type":"text/html;charset=utf8"});
-
-	res.write("user uId : " + uId);
-
-	res.write("<br>");
-
-	res.write("userd uPw : " + uPw);
-
-			
-
-	res.end();
-
-});
-
-
-
-http.createServer(app).listen(3000, function() {
-
-	console.log("server start get!!!");
-
-});
+	http.createServer(app).listen(3000, function() {
+		console.log("server start get!!!");
+	});
 
--- app2.js
+++ app2.js
@@ -1,58 +1,54 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 let request = require('request');
-const argv = require('yargs').argv;
-var fs = require("fs");
-var ejs = require("ejs");
-require('dotenv').config();
+let argv = require('yargs').argv; //도스 입력창에서 임의로 입력해주기 위해.
+let fs = require("fs");
+let ejs = require("ejs");
+  require('dotenv').config(); //개발자모드에서 환경설정 가려주는 것.
 
-const apiKey = process.env.OPEN_API_KEY;
+let apiKey = process.env.OPEN_API_KEY;
 let city = argv.c || 'seoul';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
 
-app.set('view engine', 'ejs');
+  app.set('view engine', 'ejs'); //ejs엔진세팅
+var port = 3000;
+  app.listen(port, ()=>{ //3000 포트로 서버스타트
+    console.log("Server is Starting 3000 port");
+});
 
-app.listen(3000, ()=>{
-    console.log("Server is Starting")
-})
-
-app.get('/' , (req,res ) =>{
-  request(url, function (err, response, body) {
-    if(err){
-      console.log('error:', error);
-     
-    } 
-
-      let weather = JSON.parse(body)
-      let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-      console.log(weather.weather)
-      console.log(message);
-      let result = {
-        weather : weather.main,
-        weatherMessage: message
-      }
-    
-
-    
-
-    fs.readFile('test1.html', "utf-8", (error , data)=>{
+  app.get('/', (req, res) => { //받아와라
+    request(url, (error, req, body) => {
       if(error) {
-        console.log("오류"+error);
+        console.log('error:' + error);     
+      } 
+
+        let weather = JSON.parse(body); //오브젝트로 파싱해서 받아와라
+        let celcius = Math.ceil((weather.main.temp - 32) / 1.8);
+        let message = `지금 ${weather.name}의 온도는 ${celcius}도 입니다!`;
+        console.log(weather.weather);
+        console.log(message);
+
+        let result = { //오브젝트 프로퍼티 형식으로 지정
+          weather : weather.main,
+          weatherMessage: message
+        };
+    
+    fs.readFile('test1.html', "utf-8", (error , data)=> {
+      if(error) {
+        console.log("오류" + error);
         return;
       }
 
-      res.send(
+      res.send (
         ejs.render(data, {
           data : result,
           message : message
         })
-      )
-      console.log(result)
-    })
+      );
+      console.log(result);
+    });
   });
-    
-   
-})
+});
 
 module.exports = app;
